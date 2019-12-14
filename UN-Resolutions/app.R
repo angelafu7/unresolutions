20494865163323873c2ee700ae89e446fbd9b816
angelafu7 <afu@college.harvard.edu>
2019-12-14 04:23:36 -0500

    add video

--- UN-Resolutions/app.R
+++ UN-Resolutions/app.R
@@ -46,37 +46,37 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                tabPanel("US Voting Patterns",         
                         tabsetPanel(
                             tabPanel("Overall Votes",
-                                     h5("The plot depicts the number of UN resolutions passed every year since 1986"),
+                                     h5("The plot depicts the distribution of US votes for UN resolutions"),
                                      br(),
                                      plotOutput("usavotes")
                             ),
                             tabPanel("Palestinian Conflict",
-                                     h5("The plot depicts the number of UN resolutions passed every year since 1986"),
+                                     h5("The plot depicts the distribution of US votes for UN resolutions on the Palestinian Conflict"),
                                      br(),
                                      plotOutput("usavotesme")
                             ),
                             tabPanel("Nuclear Weapons and Nuclear Material",
-                                     h5("The plot depicts the number of UN resolutions passed every year since 1986"),
+                                     h5("The plot depicts the distribution of US votes for UN resolutions on nuclear materials"),
                                      br(),
                                      plotOutput("usavotesnu")
                             ),
                             tabPanel("Arms Control and Disarmament",
-                                     h5("The plot depicts the number of UN resolutions passed every year since 1986"),
+                                     h5("The plot depicts the distribution of US votes for UN resolutions on arms control"),
                                      br(),
                                      plotOutput("usavotesdi")
                             ),
                             tabPanel("Colonialism",
-                                     h5("The plot depicts the number of UN resolutions passed every year since 1986"),
+                                     h5("The plot depicts the distribution of US votes for UN resolutions on colonialism"),
                                      br(),
                                      plotOutput("usavotesco")
                             ),
                             tabPanel("Human Rights",
-                                     h5("The plot depicts the number of UN resolutions passed every year since 1986"),
+                                     h5("The plot depicts the distribution of US votes for UN resolutions on human rights"),
                                      br(),
                                      plotOutput("usavoteshr")
                             ),
                             tabPanel("Economic Development",
-                                     h5("The plot depicts the number of UN resolutions passed every year since 1986"),
+                                     h5("The plot depicts the distribution of US votes for UN resolutions on economic development"),
                                      br(),
                                      plotOutput("usavotesec")
                             )
@@ -88,6 +88,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                             h5("These visualizations depict trends among UN resolutions since its inception"),
                             h2("Background"),
                             h5("Information about UN resolutions comes from data uploaded to Harvard by Erik Voeten, Bailey Anton, and Michael Bailey."),
+                            tags$video(src="video.mp4",width="500px",height="350px",type="video/mp4",controls="controls"),
                             h2("Creator"),
                             h5("I am a senior at Harvard College studying Government. Feel free to reach out to me at afu@college.harvard.edu with any questions.")
                         )
